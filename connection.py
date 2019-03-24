e0b6846d3607a52275b1255de5c95510943a7954
chesca <joacoemilianodf@gmail.com>
2019-03-24 17:28:31 -0300

    Un par de funciones

--- connection.py
+++ connection.py
@@ -6,6 +6,8 @@
 import socket
 from constants import *
 from base64 import b64encode
+from os import listdir
+from os.path import isfile, join, getsize
 
 class Connection(object):
     """
@@ -15,11 +17,110 @@ class Connection(object):
     """
 
     def __init__(self, socket, directory):
-        # FALTA: Inicializar atributos de Connection
-        pass
+        self.s = socket
+        self.d = './' + directory
+        self.buffer = ''
+        self.active = True # Nos dice si el cliente termino la conexión.
+
+    def _build_message(self, code, data=None):
+        """
+        Estos mensajes estan construidos por el codigo de respuesta,
+        seguida de un espacio, seguido de un mensaje de error y
+        datos del server si es que los hay.
+        """
+
+        message = str(code) + ' ' + error_messages[code] + EOL
+        if data is not None:
+            message += data
+        message += EOL # Completa el mensaje con un fin de línea.
+        return message
+
+    def send(self, message):
+        # Envia el mensaje al cliente.
+        # FALTA: Hacerlo bien.
+        self.s.send(message.encode('ascii'))
+
+    def get_file_listing(self):
+        files = [f for f in listdir(self.d) if isfile(join(self.d, f))]
+        message = self._build_message(CODE_OK, EOL.join(files) + EOL)
+        self.send(message)
+
+    def get_slice(self, filename, offset, size):
+        message = self._build_message(FILE_NOT_FOUND)
+        self.send(message)
+
+    def get_metadata(self, filename):
+        try:
+            size = getsize(self.d + '/' + filename)
+            message = self._build_message(CODE_OK, str(size))
+        except Exception:
+            message = self._build_message(FILE_NOT_FOUND)
+        self.send(message)
+
+    def quit(self):
+        message = self._build_message(CODE_OK)
+        self.send(message)
+        self.active = False
+        self.s.close()
+
+    def parser_command(self, command, args=None):
+        # Llama al metodo correspondiente al comando solicitado
+
+        print("Request: " + command)
+
+        try:
+            if command == 'get_file_listing':
+                self.get_file_listing()
+            elif command == 'get_metadata':
+                self.get_metadata(*args)
+            elif command == 'get_slice':
+                self.get_slice(*args)
+            elif command == 'quit':
+                self.quit()
+            else:
+                message = self._build_message(INVALID_COMMAND)
+                self.send(message)
+        except TypeError:
+            message = self._build_message(INVALID_ARGUMENTS)
+            self.send(message)
+
+    def _normalize_command(self, command):
+        '''
+        Ejemplo:
+        Input 'command arg arg\r\n'
+        Output [command, [arg, arg]]
+
+        Donde el primer elemento de la lista es el comando y el resto son
+        los argumentos si es que los hay.
+
+        Si no puede normalizar, devolver 100 y desconectar el cliente.
+        '''
+        if command == "" or '\n' in command:
+            message = self._build_message(BAD_EOL)
+            self.send(message)
+            self.s.close()
+        else:
+            try:
+                command, args = command.strip().split(' ', 1) 
+                return [command, args.split(' ')]
+            except ValueError:
+                return command.strip().split()
+
+    def _read_buffer(self):
+        while not EOL in self.buffer and self.active:
+            self.buffer = self.s.recv(4096).decode("ascii")
+
+            if len(self.buffer) == 0:
+                self.active = False
+        if EOL in self.buffer:
+            response, self.buffer = self.buffer.split(EOL, 1)
+            return response
+        else:
+            return ""
 
     def handle(self):
-        """
-        Atiende eventos de la conexión hasta que termina.
-        """
-        pass
+        # Atiende eventos de la conexión hasta que termina.
+        while self.active:
+            # Normaliza, descodifica mensajes.
+            command = self._normalize_command(self._read_buffer())
+            if command is not None: self.parser_command(*command)
